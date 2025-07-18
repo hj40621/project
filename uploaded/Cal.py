0a24a548d71e7b225a3084b4213747e09d6ddfcd
hj40621 <hjy2329145@naver.com>
2025-07-18 16:30:28 +0900

    새로 업로드

---
+++ uploaded/Cal.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# 1. 쥬피터노트북 파일의 이름을 Cal로 변경
+def add_two_number(num1, num2):
+    return num1 + num2
+
+def add_two_number(num1, num2):
+    return num1 - num2
+    
+def add_two_number(num1, num2):
+    return num1 * num2
+
+def add_two_number(num1, num2):
+    return num1 / num2
+
